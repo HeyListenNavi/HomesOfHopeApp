2d47d218785676995ab7842a2d0fbf6412d115f0
Verónica Guzman Ochoa <heylistennavi@proton.me>
2025-12-09 20:32:15 -0800

    add: main header view

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,46 @@
 import React from "react";
 import { View } from "react-native";
 import { Stack } from "expo-router";
-import { User2Icon, UserCircle2Icon } from "lucide-react-native";
 import "global.css";
 import { Text, TextClassContext } from "components/ui/text";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
+import * as SplashScreen from "expo-splash-screen";
+import Boxicon from "~/components/Boxicons";
+
+SplashScreen.preventAutoHideAsync();
 
 const Layout = () => {
+    const [fontsLoaded] = useFonts({
+        Boxicons: require("../assets/fonts/boxicons.ttf"),
+    });
+
+    useEffect(() => {
+        if (fontsLoaded) {
+            SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded]);
+
+    if (!fontsLoaded) return null;
+
     return (
         <Stack screenOptions={{ headerShadowVisible: false }}>
             <Stack.Screen
                 name="(tabs)"
                 options={{
                     header: () => (
-                        <View className="flex flex-row items-center justify-between px-3 pt-4 pb-6 bg-white rounded-b-3xl">
-                            <Text className="font-bold text-2xl m-0" variant="h1">Hope</Text>
-                            <UserCircle2Icon size={28} className="stroke-primary"/>
+                        <View className="flex flex-row items-center justify-between px-6 pt-4 pb-6 bg-white rounded-b-3xl">
+                            <Text
+                                className="font-bold text-2xl m-0"
+                                variant="h1"
+                            >
+                                Hope
+                            </Text>
+                            <Boxicon
+                                name="bxs-user-circle"
+                                size={38}
+                                color="#61b346"
+                            />
                         </View>
                     ),
                 }}
