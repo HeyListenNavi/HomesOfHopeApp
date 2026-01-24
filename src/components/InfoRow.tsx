f7d062bf160c178037bd1776a415cdfac8423839
Verónica Guzman Ochoa <heylistennavi@proton.me>
2026-01-24 03:45:11 -0800

    style: add icon to info row component

--- src/components/InfoRow.tsx
+++ src/components/InfoRow.tsx
@@ -1,25 +1,31 @@
 import { View } from "react-native";
-import { BoxIconName } from "@/components/Boxicons";
+import Boxicon, { BoxIconName } from "@/components/Boxicons";
 import { Text } from "@/components/ui/text";
 
 const InfoRow = ({
     label,
     value,
+    icon,
+    iconColor = "#9ca3af",
     description,
     className,
 }: {
     label: string;
     value?: string | null | boolean;
     icon?: BoxIconName;
+    iconColor?: string;
     description?: string;
     className?: string;
 }) => {
     return (
-        <View className="flex-row gap-3 items-start">
-            <View className="gap-0.5">
+        <View className="flex-row gap-1 items-start">
+            {icon && <Boxicon name={icon} color={iconColor} size={18} />}
+            <View className="gap-1">
                 <Text className="text-gray-400 text-sm uppercase">{label}</Text>
                 <View>
-                    <Text className={`text-gray-600 font-bold ${className}`}>{value}</Text>
+                    <Text className={`text-gray-600 font-bold ${className}`}>
+                        {value}
+                    </Text>
                     {description && (
                         <Text className="text-gray-400 text-sm">
                             {description}
