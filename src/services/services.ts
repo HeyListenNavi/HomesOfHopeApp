b1933e4e3f194f390d4063682f6e15c2aab575ca
Verónica Guzman Ochoa <heylistennavi@proton.me>
2026-02-04 16:01:27 -0800

    add: rest of crud services

--- src/services/services.ts
+++ src/services/services.ts
@@ -1,6 +1,23 @@
 import { CrudService } from "@/services/crudService";
-import { FamilyProfile, Task, Visit } from "@/types/api";
+import {
+    FamilyProfile,
+    FamilyMember,
+    Visit,
+    Task,
+    Note,
+    Document,
+    Testimony,
+    User,
+} from "@/types/api";
 
 export const familyService = new CrudService<FamilyProfile>("/family-profiles");
+export const familyMemberService = new CrudService<FamilyMember>(
+    "/family-members",
+);
 export const visitService = new CrudService<Visit>("/visits");
 export const taskService = new CrudService<Task>("/tasks");
+export const userService = new CrudService<User>("/users");
+
+export const noteService = new CrudService<Note>("/notes");
+export const documentService = new CrudService<Document>("/documents");
+export const testimonyService = new CrudService<Testimony>("/testimonies");
